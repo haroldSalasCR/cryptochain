9b5c81517d83f686a5de78c948a744afb6a70762
hsalas006 <hsalas006@github.com>
2020-09-22 00:10:46 -0600

    TDD: Genesis Block and mineBlock functions

--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const { GENESIS_DATA } = require("./config");
+
 class Block {
   constructor({ timestamp, lastHash, hash, data }) {
     this.timestamp = timestamp;
@@ -5,13 +7,18 @@ class Block {
     this.hash = hash;
     this.data = data;
   }
+
+  static genesis() {
+    return new this(GENESIS_DATA);
+  }
+
+  static minedBlock({ lastBlock, data }) {
+    return new this({
+      timestamp: Date.now(),
+      lastHash: lastBlock.hash,
+      data,
+    });
+  }
 }
 
-const block1 = new Block({
-  timestamp: "21/09/2021",
-  lastHash: "foo-lastHash",
-  hash: "foo-hash",
-  data: "foo-data",
-});
-
-console.log("block1", block1);
+module.exports = Block;
